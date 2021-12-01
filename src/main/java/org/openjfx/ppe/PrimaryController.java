ba7fde3142ca87372207342a598844d23015287d
Benjamin <drouinbenjamin20@gmail.com>
2021-12-01 09:12:30 +0100

    modification du formulaire d'entrée dans la salle de sport

--- src/main/java/org/openjfx/ppe/PrimaryController.java
+++ src/main/java/org/openjfx/ppe/PrimaryController.java
@@ -1,12 +1,28 @@
 package org.openjfx.ppe;
 
 import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
 public class PrimaryController {
+	
+	//private Button submit;
+	@FXML
+	private ComboBox<String> choix;
+	ObservableList<String> choixList = FXCollections.observableArrayList("Musculation", "Fitnesse");
+	
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        //App.setRoot("secondary");
     }
+    
+    @FXML
+    private void initialize() {
+    	choix.setItems(choixList);
+    }
+
 }
